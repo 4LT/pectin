cbc65bb090e8c2fe8f51a7688d272a99d881ddaf
seth <rader.seth@gmail.com>
2024-02-25 16:42:27 -0500

    Fix build script warning

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::process::Command;
 
 fn warn(mesg: impl std::fmt::Display) {
-    eprintln!("[WARN] {mesg}");
+    println!("cargo:warning={mesg}");
 }
 
 fn main() {
@@ -16,24 +16,27 @@ fn main() {
         .expect("Failed to run \"cargo about generate\"");
 
     let license_dict = if out.status.success() {
-        warn("\"cargo about\" failed (not installed?)");
         String::from_utf8_lossy(&out.stdout)
             .replace("&quot;", r#"\""#)
             .replace("&lt;", "<")
             .replace("&gt;", ">")
             .replace("&#x27;", "\x27")
     } else {
+        warn("\"cargo about\" failed (not installed?)");
+        warn("Skipping compilation of licenses");
         String::from("")
     };
 
     fs::write("src/licenses.tcldict", license_dict).unwrap();
 
     let version = env!("CARGO_PKG_VERSION");
+    let repo = env!("CARGO_PKG_REPOSITORY");
 
     fs::write(
         "src/build.tcldict",
         format!(
             r#"version {version}
+            repo {repo}
 "#
         ),
     )
