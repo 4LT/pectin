630f5d6cf430a9fc55c4e1e9f71c2b2774ee6557
seth <rader.seth@gmail.com>
2024-02-26 15:09:13 -0500

    Clean up statement order

--- build.rs
+++ build.rs
@@ -9,8 +9,9 @@ fn warn(mesg: impl std::fmt::Display) {
 
 fn main() {
     println!("cargo:rerun-if-changed=.git");
-    git_testament!(TESTAMENT);
     println!("cargo:rerun-if-changed=about.hbs");
+
+    git_testament!(TESTAMENT);
 
     let out = Command::new("cargo")
         .arg("about")
