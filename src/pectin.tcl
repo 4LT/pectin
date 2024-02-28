4c0767a5b711eaf9674315f5de1dbf62fac80539
seth <rader.seth@gmail.com>
2024-02-28 17:48:17 -0500

    Clear 'alternate' on filter checkbox

--- src/pectin.tcl
+++ src/pectin.tcl
@@ -76,10 +76,10 @@ proc refreshPaths {} {
 }
 
 proc failureFilterUpdate {} {
-    if {[lsearch -exact [.filter state] selected] == -1} {
-        dict set ::config failureOnly 0
-    } else {
+    if {[.filter instate selected]} {
         dict set ::config failureOnly 1
+    } else {
+        dict set ::config failureOnly 0
     }
 
     refreshConfig
@@ -289,6 +289,7 @@ ttk::treeview .maps -columns {state failure}\
 
 ttk::checkbutton .filter -text "Show failures only" -padding {8 8 8 8}\
     -command failureFilterUpdate
+.filter state !alternate
 
 grid .filter -sticky ew -columnspan 2
 grid .maps .scrolly -sticky nesw
